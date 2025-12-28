b14d67bc5d3b0601f0fb04de1508c55fc91dd03d
sjthesahiljoseph <sjthesahiljoseph@gmail.com>
2025-12-29 02:59:16 +0500

    pixel by pixel blue but why not red

--- TwentyFourJuneNight/win32_platform.cpp
+++ TwentyFourJuneNight/win32_platform.cpp
@@ -1,9 +1,21 @@
 
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
 
 // TODO(SJtheSahilJoseph): This is global variable for now.
 global_variable bool Running = true;
@@ -43,6 +55,35 @@ internal void Win32ResizeDIBSection(int Width, int Height)
 	int BitmapMemorySize = ((BitmapWidth * BitmapHeight) * BytesPerPixel);
 
 	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+	int Pitch = Width * BytesPerPixel;
+
+	uint8* Row = (uint8*)BitmapMemory;
+
+	for (int Y = 0; Y < BitmapHeight; Y++)
+	{
+
+		uint8* Pixel = (uint8*)Row;
+
+		for (int X = 0; X < BitmapWidth; X++)
+		{
+
+			*Pixel = 255;
+			Pixel++;
+
+			*Pixel = 0;
+			Pixel++;
+
+			*Pixel = 0;
+			Pixel++;
+
+			*Pixel = 0;
+			Pixel++;
+
+		}
+
+		Row += Pitch;
+	}
 
 }
 
